c615e6af0c60c6d849a87f63b9a1a8cda1f9f4d1
herbert159 <hipolito160@gmail.com>
2024-06-07 15:46:06 -0300

    remondo comentaios

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from routers import router
 
-#api link = https://dadosabertos.camara.leg.br/swagger/api.html
-
 app = FastAPI(
     title="Insight - Processo seletivo",
-    description="Desafio do processo seletivo para vaga de Back-end Python. A API pública a ser é a https://dadosabertos.camara.leg.br/swagger/api.html",
+    description="Desafio do processo seletivo para vaga de Full-stack Python. A API pública a ser consumida é a https://dadosabertos.camara.leg.br/swagger/api.html",
     version="0.0.1"
 )
 
--- schemas/deputado.py
+++ schemas/deputado.py
@@ -5,9 +5,7 @@ class Deputado(BaseModel):
     nome:str
     id:int
     idLegislatura:int
-#    telefone:Optional[str] = None
     siglaUf:str
     siglaPartido:str
     urlFoto:str
     email:str | None = None
-#    situacao:Optional[str] = None
